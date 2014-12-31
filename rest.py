0c7c992361cffd5dd5d7163730fda90b36d6a103
Gautam <gautam.madaan@gmail.com>
2014-12-30 16:21:35 -0800

    Gautam's mac Sum working

--- rest.py
+++ rest.py
@@ -11,19 +11,20 @@ class vistrails:
 		i = web.data()
 		print i
 		subprocess.call(['rm','-f','nodes.json'])
-		subprocess.call(['rm','-f','/Users/chen/Desktop/result.txt'])
+		subprocess.call(['rm','-f','/Users/gautammadaan/Desktop/result.txt'])
 
 		with open('nodes.json', 'w') as f:
 			f.write(i)
 
 		subprocess.call(['python','translate.py'])
-		subprocess.call(['python','/Users/chen/Desktop/vistrials/vistrails/run.py','-b','/Users/chen/Desktop/web-vt/nodes-to-vt/output.xml'])
+		subprocess.call(['python','/Users/gautammadaan/Downloads/vistrails-src/vistrails/run.py','-b','/Users/gautammadaan/Downloads/nodes-to-vt/output.xml'])
 
-		with open('/Users/chen/Desktop/result.txt', 'r') as f:
+		with open('/Users/gautammadaan/Desktop/result.txt', 'r') as f:
 			s = f.read()
 			f.close()
 
 		web.header('Access-Control-Allow-Origin', '*')
+		print s
 		return s
 
 if __name__ == '__main__':
